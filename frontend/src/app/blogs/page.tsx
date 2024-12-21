df0a305d93022c17f725ad8b2d3442b56824a043
Bhavesh <bhaveshshah9595@gmail.com>
2024-12-21 16:00:58 +0530

    Bhavesh: adding frontend for blogs

--- frontend/src/app/blogs/page.tsx
+++ frontend/src/app/blogs/page.tsx
@@ -1,8 +1,33 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { BASE_API_URL } from "@/constant";
 
-export default function blogs() {
+export default async function blogs() {
+
+  //Getting blogs from the backend
+  const raw = await fetch(BASE_API_URL + 'blog');
+  const blogs = await raw.json();
+  console.log(blogs);
+
   return (
-    <h1>Blogs</h1>
+    <div>
+      <h1>Blogs</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Sr</th>
+            <th>Titles</th>
+          </tr>
+        </thead>
+        <tbody>
+          {blogs.map((blog:any) => (
+            <tr key={blog.id}>
+              <td>{blog.id}</td>
+              <td>{blog.title}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
